420615dcacaed7441009e7f53de6e7ba614815d4
yoshikin-tomomura <yoshikin-tomomura@proton.me>
2023-12-06 06:53:37 +0900

    first commit

---
+++ 目次.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+
+st.title("池田糖化工業㈱裏サイト入口")
+
+st.markdown("""
+
+
+
+
+
+##
+##
+##
+##
+##
+## リンク先一覧表
+##
+
+01.   [池田糖化工業㈱裏サイト](https://wwwikeda.streamlit.app)
+
+02.   [池田糖化工業㈱裏の裏サイト](https://wwwikedatohkacojp.streamlit.app)
+
+03.   [池田糖化工業㈱裏の裏の裏サイト](https://bnxbbypxtbemtbouhgaqew.streamlit.app/)
+
+04.   [池田糖化工業㈱裏の裏の裏の裏サイト](https://ikedaikeda.streamlit.app/)
+
+05.   [池田糖化工業㈱ブログ](https://ytetsu.pythonanywhere.com/)
+
+--------------------------------------------------------------------------------------------------
+
+06.  [池田糖化工業㈱裏サイト1](https://oosaka.pythonanywhere.com/accounts/login/)
+
+07.  [池田糖化工業㈱裏サイト2](https://koujitaniguchi.pythonanywhere.com/accounts/login/)
+
+08.  [池田糖化工業㈱裏サイト3](https://taniimasakazu.pythonanywhere.com/accounts/login/)
+
+09.  [池田糖化工業㈱裏サイト4](https://masakazutanii.pythonanywhere.com/accounts/login/)
+
+10.  [池田糖化工業㈱日報](https://kouji.pythonanywhere.com/accounts/login/)
+
+
+
+""")
