613158cf7f51d6ffbfc9f6554a690b5607a2006b
yoshikin-tomomura <yoshikin-tomomura@proton.me>
2024-02-14 13:57:09 +0900

    first commit

---
+++ 目次.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+st.markdown("""
+##
+## 池田糖化工業㈱裏サイト入口リンク先
+01.  [池田糖化工業㈱裏サイト](https://wwwikeda.streamlit.app)
+02.  [池田糖化工業㈱裏の裏サイト](https://wwwikedatohkacojp.streamlit.app)
+03.  [池田糖化工業㈱裏の裏の裏サイト](https://bnxbbypxtbemtbouhgaqew.streamlit.app/)
+04.  [池田糖化工業㈱裏の裏の裏の裏サイト](https://ikedaikeda.streamlit.app/)
+05.  [池田糖化工業㈱ブログ](https://ytetsu.pythonanywhere.com/)
+06.  [池田糖化工業㈱裏サイト1](https://oosaka.pythonanywhere.com/accounts/login/)
+07.  [池田糖化工業㈱裏サイト2](https://koujitaniguchi.pythonanywhere.com/accounts/login/)
+08.  [池田糖化工業㈱裏サイト3](https://taniimasakazu.pythonanywhere.com/accounts/login/)
+09.  [池田糖化工業㈱裏サイト4](https://masakazutanii.pythonanywhere.com/accounts/login/)
+10.  [池田糖化工業㈱日報](https://kouji.pythonanywhere.com/accounts/login/)
+##
+## 池田糖化工業㈱グループ会社HPリンク先
+01.  [池田糖化工業㈱](https://nakagawatomohiro.pythonanywhere.com/nippo/www.ikedatohka.co.jp/)
+02.  [キサイフーズ工業㈱](https://kisaifoods.pythonanywhere.com/nippo/Kisaifoods/)
+03.  [上越フーズ㈱](https://joetsufoods.pythonanywhere.com/nippo/Joetsufoods/)
+04.  [タヌマフーズ㈱](https://tanumafoods.pythonanywhere.com/nippo/tanumafoods/)
+05.  [日本リコス㈱](https://ricosu.pythonanywhere.com/nippo/ricosu/)
+06.  [岡山アグリフーズ㈱](https://okayamaagrifoods.pythonanywhere.com/nippo/okayama-agrifoods/)
+07.  [北海道ソラサンフーズ㈱](https://sorasun.pythonanywhere.com/nippo/sorasun/)
+08.  [CARAMELIFE㈱](https://caramelife.pythonanywhere.com/nippo/caramelife/)
+09.  [池田糖化工業㈱イントラネット](https://tomohironakagawa.pythonanywhere.com/accounts/login/)
+""")
+
