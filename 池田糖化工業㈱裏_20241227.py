a832388c50a8d05ebda8dc7be7e97a754b02a1fe
yoshikin-tomomura <yoshikin-tomomura@proton.me>
2024-12-27 10:02:24 +0900

    first commit

---
+++ 池田糖化工業㈱裏_20241227.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+st.markdown("""
+##
+## 池田糖化工業㈱裏サイトリンク集
+
+##
+1.  [池田糖化工業㈱裏サイト](https://ikeda1.streamlit.app)
+
+2.  [池田糖化工業㈱裏の裏サイト](https://ikeda2.streamlit.app)
+
+3.  [池田糖化工業㈱裏の裏の裏サイト](https://ikeda3.streamlit.app)
+
+4.  [池田糖化工業㈱裏の裏の裏の裏サイト](https://ikeda4.streamlit.app/)
+
+5.  [池田糖化工業㈱イントラネット](https://tomohironakagawa.pythonanywhere.com/accounts/login/)
+
+""")
+
+
