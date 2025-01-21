77a5e47b8d24b9d223bdf666538876ee94f7aa36
yoshikin-tomomura <yoshikin-tomomura@proton.me>
2025-01-21 13:31:21 +0900

    first commit

---
+++ 池田糖化工業㈱裏.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.markdown('<p style="font-family:system-ui; color:blue; font-size: 52px;">池田糖化工業㈱裏サイトリンク集</p>', unsafe_allow_html=True)
+
+st.markdown("---") #区切り線
+st.markdown("")
+
+
+st.link_button("1.　池田糖化工業㈱裏サイト", "https://ikeda1.streamlit.app",use_container_width=True,type="secondary" )
+st.markdown("")
+st.link_button("　　2.　池田糖化工業㈱裏の裏サイト", "https://ikeda2.streamlit.app",use_container_width=True,type="secondary")
+st.markdown("")
+st.link_button("　　　　3.　池田糖化工業㈱裏の裏の裏サイト", "https://ikeda3.streamlit.app",use_container_width=True,type="secondary")
+st.markdown("")
+st.link_button("　　　　　　4.　池田糖化工業㈱裏の裏の裏の裏サイト", "https://ikeda4.streamlit.app",use_container_width=True,type="secondary")
+
+
+
+
+#st.markdown("""
+##
+# 池田糖化工業㈱裏サイトリンク集
+
+##
+# 1.  [池田糖化工業㈱裏サイト](https://ikeda1.streamlit.app)
+
+## 2.  [池田糖化工業㈱裏の裏サイト](https://ikeda2.streamlit.app)
+
+### 3.  [池田糖化工業㈱裏の裏の裏サイト](https://ikeda3.streamlit.app)
+
+#### 4.  [池田糖化工業㈱裏の裏の裏の裏サイト](https://ikeda4.streamlit.app/)
+
+
+#""")
